ba4eaec063cfb38a0555f62fad4980fb3b8ec77d
Karthik D <ndkl2498@gmail.com>
2025-12-01 19:47:06 +0530

    chore: add package-info for sub packages

---
+++ src/main/java/io/kestra/plugin/klaviyo/campaign/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Campaign",
+    description = "Tasks for managing Klaviyo campaigns and recipient estimations.",
+    categories = PluginSubGroup.PluginCategory.TOOL
+)
+package io.kestra.plugin.klaviyo.campaign;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
